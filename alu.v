eef68a35a68dab83faa2b4d2967b75c64edb8011
Aden <adenjo@ucsb.edu>
2024-10-02 20:31:48 -0700

    made new line after endmodule and changed line 25: frmo 32 to 31

--- alu.v
+++ alu.v
@@ -1,28 +1,30 @@
-module alu(input [31:0] a, b,
-           input [2:0] f,
-           output [31:0] result,
-           output zero,
-           output overflow,
-           output carry,
-           output negative);
-    
+module alu(
+    input  [31:0] a, b,
+    input  [2:0]  f,
+    output [31:0] result,
+    output        zero,
+    output        overflow,
+    output        carry,
+    output        negative
+);
     wire [31:0] b_tmp;
+    wire [32:0] sum_ext;
     wire [31:0] sum;
     wire of;
 
     assign b_tmp = (f[0] == 1'b1) ? ~b : b;
-    assign sum = a + b_tmp + f[0];
+    assign sum_ext = {1'b0, a} + {1'b0, b_tmp} + f[0];
+    assign sum = sum_ext[31:0]; 
     assign of = ~(a[31] ^ b[31] ^ f[0]) & (a[31] ^ sum[31]) & ~f[1];
-
-    assign result = (f == 3'b000 || f == 3'b001) ? sum[31:0] :
+    assign result = (f == 3'b000 || f == 3'b001) ? sum :
                     (f == 3'b010) ? (a & b) :
                     (f == 3'b011) ? (a | b) :
                     (f == 3'b101) ? {31'b0, of ^ sum[31]} :
                     32'b0;
-    
-    assign zero = &(~result);
+
+    assign zero = (result == 32'b0);
     assign overflow = of;
-    assign carry = (f[0] == 1'b1) ? (~a & b) | ((~a | b) & sum[31]) : sum[31] & ~f[1];
+    assign carry = sum_ext[32] & ~f[1];
     assign negative = result[31];
 
 endmodule
